fe5e04800f6ebf5dcb42e9e7f6629f69b2d354cb
RutraSan <karabekov.artur@gmail.com>
2022-04-13 17:01:11 +0300

    init vs project

---
+++ Hamsa/Main.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Hamsa
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CameraBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CameraBox
+            // 
+            this.CameraBox.Location = new System.Drawing.Point(-2, -1);
+            this.CameraBox.Name = "CameraBox";
+            this.CameraBox.Size = new System.Drawing.Size(799, 453);
+            this.CameraBox.TabIndex = 0;
+            this.CameraBox.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CameraBox);
+            this.Name = "Main";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CameraBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CameraBox;
+    }
+}
+
---
+++ Hamsa/Main.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//EMGU
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+//DiresctShow
+//using DirectShowLib;
+
+namespace Hamsa
+{
+    public partial class Main : Form
+    {
+
+        #region Variables
+        #region Camera Capture Variables
+        private VideoCapture _capture = null; //Camera
+        private bool _captureInProgress = false; //Variable to track camera state
+        int CameraDevice = 0; //Variable to track camera device selected
+        //Video_Device[] WebCams; //List containing all the camera available
+        #endregion
+        #region Camera Settings
+        int Brightness_Store = 0;
+        int Contrast_Store = 0;
+        int Sharpness_Store = 0;
+        #endregion
+        #endregion
+
+        public Main()
+        {
+            InitializeComponent();
+            _capture = new VideoCapture();
+            _capture.ImageGrabbed += ProcessFrame;
+            _capture.FlipHorizontal = true;
+            _capture.Start();
+
+        }
+
+        /// <summary>
+        /// Handle of the <c>ImageGrabbed</c> Event.
+        /// responsible for the getting the image from <c>_capture</c>.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void ProcessFrame(object sender, EventArgs args)
+        {
+            Mat image = new Mat();
+            if( _capture.Retrieve(image))
+            {
+                CameraBox.Image = (image.ToImage<Bgr, Byte>()).ToBitmap();
+            }
+        }
+
+
+        
+
+    }
+}
