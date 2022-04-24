f59e58350edc8068175dbdb116fee321129433af
RutraSan <karabekov.artur@gmail.com>
2022-04-24 12:35:47 +0300

    Preferences class

---
+++ Hamsa/Preferences.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamsa
+{
+    enum Fingers
+    {
+        Thumb,
+        Index,
+        Middle,
+        Ring,
+        Pinky
+    }
+    class Preferences
+    {
+
+        public Fingers mouse { get; set; }
+
+        public Preferences()
+        {
+            mouse = Fingers.Index;
+        }
+    }
+}
